bf5963618e276381a4e007f2570f67af7fa118ef
Garry but Spinning <64482863+garryspins@users.noreply.github.com>
2020-05-11 00:06:29 -0700

    Create hello.lua

---
+++ hello.lua
@@ -0,0 +1,14 @@
+function RunHello(str)
+	local sttrrr = ""
+	local error = false
+	for i=1, #str do
+		local char = str:sub(i,i)
+		if char == "h" then
+			sttrrr = sttrrr .. "Hello World"
+		else 
+			error = true 
+		end
+	end
+	if error then sttrrr = "ERROR!" end
+	return sttrrr
+end
