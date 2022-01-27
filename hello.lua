0c94ec21d67c6ea0e5df8c29458a3cbd17b3f34a
gayryspins <64482863+garryspins@users.noreply.github.com>
2022-01-27 10:48:35 -0800

    Optimized Very Uber Complex Implementation!

--- hello.lua
+++ hello.lua
@@ -1,14 +1,8 @@
+
 function RunHello(str)
-	local sttrrr = ""
-	local error = false
-	for i=1, #str do
-		local char = str[i]
-		if char == "h" then
-			sttrrr = sttrrr .. "Hello World"
-		else 
-			error = true 
-		end
+	local toret = ""
+	for i=1,#str do
+		if str[i] == "h" then toret = toret .. "Hello World" else return "ERROR!" end
 	end
-	if error then sttrrr = "ERROR!" end
-	return sttrrr
+	return toret
 end
